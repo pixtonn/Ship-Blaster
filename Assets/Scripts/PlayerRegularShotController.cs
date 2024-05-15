d27457f49bfaff9d4a6a019641ad011878b23d7c
pixtonn <82233877+pixtonn@users.noreply.github.com>
2024-05-15 16:07:36 -0600

    Added player shooting
    Player can shoot by pressing or holding down the Space key. The shots go up on the screen and disappear after leaving the screen.

---
+++ Assets/Scripts/PlayerRegularShotController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRegularShotController : MonoBehaviour
+{
+    public float speed = 30.0f;
+
+    private float offsetContinue = 10.0f; // how much further past the edge of the map the shot goes, makes sure it doesn't disappear prematurely on screen
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+
+        // destroy itself if out of bounds
+        if (transform.position.z > GlobalVariables.top+offsetContinue)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
